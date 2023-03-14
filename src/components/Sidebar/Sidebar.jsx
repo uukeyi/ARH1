4ecd77ff415801ec280895de0916ec4aab7a8d5b
unknown <kozhon.amantay@gmail.com>
2023-03-15 01:11:12 +0600

    make header and footer for pc

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Sidebar = () => {
+  return <div></div>;
+};
+const SidebarWrapper = styled.div``;
+export default Sidebar;
