c7973d928c6806e354cf516bf1107fbbcb6db830
Aleksandr <119818692+uukeyi@users.noreply.github.com>
2023-03-24 03:20:25 +0300

    fixing sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,56 +1,67 @@
-import React from 'react';
-import styles from './Sidebar.module.css';
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Sidebar.module.css";
 interface SidebarProps {
-  isOpen: boolean;
-  setIsOpen: any;
+   isOpen: boolean;
+   setIsOpen: any;
 }
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, setIsOpen }) => {
-  const {
-    wrapper,
-    container,
-    logoLink,
-    logoImg,
-    navWrapper,
-    navLink,
-    numberButton,
-    blur,
-    active,
-    close,
-  } = styles;
-  const classList = `${wrapper} ${active}`;
-  return (
-    <div className={isOpen ? classList : wrapper}>
-      <button
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-        className={close}
-      />
-      <div className={container}>
-        <a href="#" className={logoLink}>
-          <img
-            className={logoImg}
-            src="https://arh.yamaster.ml/images/arhlogotip02.webp"
-            alt="logo"
-          />
-        </a>
-        <nav className={navWrapper}>
-          <a href="" className={navLink}>
-            О НАС
-          </a>
-          <a href="" className={navLink}>
-            НАШИ РАБОТЫ
-          </a>
-          <a href="" className={navLink}>
-            БАЗА ЗНАНИЙ
-          </a>
-          <a href="" className={navLink}>
-            КОНТАКТЫ
-          </a>
-          <a className={numberButton}>8 (812) 970-90-05</a>
-        </nav>
-      </div>
-    </div>
-  );
+   const {
+      wrapper,
+      container,
+      logoLink,
+      logoImg,
+      navWrapper,
+      navLink,
+      numberButton,
+      blur,
+      active,
+      close,
+      activeLink,
+      bg
+   } = styles;
+   const classList = `${wrapper} ${active}`;
+
+   return (
+ 
+         <div className={isOpen ? classList : wrapper}>
+            <button
+               onClick={() => {
+                  setIsOpen(!isOpen);
+               }}
+               className={close}
+            />
+            <div className={container}>
+               <a href="#" className={logoLink}>
+                  <img
+                     className={logoImg}
+                     src="https://arh.yamaster.ml/images/arhlogotip02.webp"
+                     alt="logo"
+                  />
+               </a>
+               <nav className={navWrapper}>
+                  <a href="" className={navLink}>
+                     О НАС
+                  </a>
+                  <NavLink
+                     to={"ourProjects"}
+                     className={({ isActive }) =>
+                        isActive ? `${navLink} ${activeLink}` : navLink
+                     }
+                  >
+                     НАШИ РАБОТЫ
+                  </NavLink>
+                  <a href="" className={navLink}>
+                     БАЗА ЗНАНИЙ
+                  </a>
+                  <a href="" className={navLink}>
+                     КОНТАКТЫ
+                  </a>
+                  <a className={numberButton}>8 (812) 970-90-05</a>
+               </nav>
+            </div>
+         </div>
+
+   );
 };
 export default Sidebar;
