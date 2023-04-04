b3b4c19142af385b90656b287e46a48f7221fd64
unknown <kozhon.amantay@gmail.com>
2023-04-04 10:01:00 +0600

    change index.tsx component

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-// import ReactDOM from "react-dom/client";
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-// const root = ReactDOM.createRoot(
-//    document.getElementById("root") as HTMLElement
-// );
-const root = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  root
+  </BrowserRouter>
 );
